6ee4b913fae011b825abfd7f6a0652c3c840b37c
miabhishekgaikwad <abhishekgaikwad065@gmail.com>
2025-02-18 11:54:15 +0530

    update

---
+++ script.js
@@ -0,0 +1,7 @@
+let feet = document.getElementById("feet")
+let inch = document.getElementById("inch")
+let weight = document.getElementById("weight")
+
+let metre = feet * 0.3048;
+let metreToInch = inch * 0.0254;
+console.log(metre)
