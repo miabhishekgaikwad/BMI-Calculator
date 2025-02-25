349ee4c37db04365d72f143f322857a92ac23699
miabhishekgaikwad <abhishekgaikwad065@gmail.com>
2025-02-25 11:18:17 +0530

    update

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
+let calcBtn = document.getElementById("calcBtn");
 
-let feet = document.getElementById("feet")
-let inch = document.getElementById("inch")
-let weight = document.getElementById("weight")
+calcBtn.addEventListener("click", () => {
+  // sabhi input ki value lena
+  let feet = Number(document.getElementById("feet").value);
+  let inch = Number(document.getElementById("inch").value);
+  let weight = Number(document.getElementById("weight").value);
+
+  // feet aur inch ko meters me convert karna
+  feetToMeters = 0.3048 * feet;
+  inchToMeters = 0.0254 * inch;
+  meters = feetToMeters + inchToMeters;
+
+  // feet ke aur inch ke meters ka square
+
+  height = meters * meters;
+
+  // bmi calculet karna
+
+  bmi =  weight / height;
+
+  
+  // bmi ko page par show karna
+
+  document.getElementById("bmiValue").innerHTML = `BMI = ${Math.floor(bmi)}`
 
 
-let calcBtn = document.querySelector(".calcBtn")
-
-calcBtn.addEventListener("click", ()=>{
-  console.log(feet.value)
-})
+  console.log(feetToMeters, inchToMeters, meters, weight,"height: ",height ,"bmi:", bmi);
+});
